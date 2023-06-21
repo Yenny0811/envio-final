102efbbcd7d9400e5bfcf743917afb7654516066
Jander Buritica <janderburitica123@gmail.com>
2023-06-21 17:56:07 -0500

    ok

---
+++ data/platosRestaurante.py
@@ -0,0 +1,30 @@
+menu1=["Muelas de cangrejo",65000,"entrada"]
+menu2=["Langosta al ajillo",120000,"platofuerte"]
+manu3=["Muse de chocolate",25000, "postre"]
+menu4=["Rollitos parmesanos",35000, "entrada"]
+menu5=["Lasagna 3 carnes",45000 , "platofuerte"]
+menu6=["Tiramizu",20000, "postre"]
+menu7=["Picada Criolla",30000, "entrada"]
+menu8=["Cazuela Paisa",50000, "platofuerte"]
+menu9=["Miguelucho",8000, "postre"]
+menu10=["Ceviche",20000, "entrada"]
+menu11=["Robalo Apanado",33000, "platofuerte"]
+menu12=["Cocada",15000, "postre"]
+menu13=["Entrada sinaloa",30000, "entrada"]
+menu14=["Ilusion de tacos",100000, "platofuerte"]
+menu15=["Totopos dulces",12000, "postre"]
+menu16=["Entraña de res",25000, "entrada"]
+menu17=["Bife al cuartil",150000, "platofuerte"]
+menu18=["Helado de Vainilla ",12000, "postre"]
+menu19=["Frutillas al bosque",20000, "entrada"]
+menu20=["Risotto de amaronto y hongos",45000, "platofuerte"]
+menu21=["Muse de aguacate",20000, "postre"]
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
